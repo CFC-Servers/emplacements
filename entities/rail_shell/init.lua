b758a522c39f526bf93472911e603a5d012ac726
StrawWagen <64710817+StrawWagen@users.noreply.github.com>
2021-07-31 13:01:16 -0700

    changed many things again
    buffed turret_grenade's projectile ( turret_40mm_frag ), blast damage from 45 to 90, blast range 400 to 350
    
    reworked turret_bullet2 a bit, changed bullet damage to 70, and added a 20 damage explosion on bullet hit.
    turret_bullet2 also creates a decal to communicate that yes, it does explode
    
    added some comments that explain the variable "concrete"
    also did heavy formatting in both projectile's code, everything should be less shit, and overall easier to read while still being consistent.

---
+++ entities/rail_shell/init.lua
@@ -0,0 +1,114 @@
+AddCSLuaFile( "cl_init.lua" )
+AddCSLuaFile( "shared.lua" )
+include('shared.lua')
+
+function ENT:Initialize()   
+	
+	self.flightvector = self.Entity:GetForward() * 300
+	self.timeleft = CurTime() + 5
+	
+	self.AirburstTime = CurTime() + 5 
+	self.Entity:SetModel( "models/weapons/w_missile_closed.mdl" )
+	self.Entity:PhysicsInit( SOLID_VPHYSICS )	  -- Make us work with physics,  	
+	self.Entity:SetMoveType( MOVETYPE_NONE )   --after all, gmod is a physics  	
+	self.Entity:SetSolid( SOLID_VPHYSICS )		-- CHEESECAKE!	>:3		   
+	
+	Tracer = ents.Create("env_spritetrail")
+	Tracer:SetKeyValue("lifetime","0.1")
+	Tracer:SetKeyValue("startwidth","90")
+	Tracer:SetKeyValue("endwidth","15")
+	Tracer:SetKeyValue("spritename","trails/laser.vmt")
+	Tracer:SetKeyValue("rendermode","5")
+	Tracer:SetKeyValue("rendercolor","37 138 210")
+	Tracer:SetPos(self.Entity:GetPos())
+	Tracer:SetParent(self.Entity)
+	Tracer:Spawn()
+	Tracer:Activate()
+
+	Glow = ents.Create("env_sprite")
+	Glow:SetKeyValue("model","orangecore2.vmt")
+	Glow:SetKeyValue("rendercolor","37 138 210")
+	Glow:SetKeyValue("scale","0.4")
+	Glow:SetPos(self.Entity:GetPos())
+	Glow:SetParent(self.Entity)
+	Glow:Spawn()
+	Glow:Activate()
+	
+end   
+
+ function ENT:Think()
+	
+	if self.timeleft < CurTime() then
+		self.Entity:Remove()				
+
+	end
+
+	if self.AirburstTime < CurTime() then
+		local owner = IsValid( self:GetOwner() ) and self:GetOwner()
+		local inflictor = owner or self.Turret
+		util.BlastDamage(inflictor, self.Turret, self.Entity:GetPos(), 700, 100)
+		local effectdata = EffectData()
+		effectdata:SetOrigin(self.Entity:GetPos())
+		effectdata:SetScale(2)
+		effectdata:SetMagnitude(20)
+		util.Effect( "gdca_airburst_t", effectdata )
+		self.Entity:Remove()
+		
+	end
+
+	local trace = {}
+	trace.start 	= self.Entity:GetPos()
+	trace.endpos 	= self.Entity:GetPos() + self.flightvector
+	trace.filter 	= self.Entity 
+	trace.mask 	= MASK_SHOT + MASK_WATER			// Trace for stuff that bullets would normally hit
+	local tr = util.TraceLine( trace )
+	
+	if tr.Hit then
+		
+		if tr.HitSky then
+			self.Entity:Remove()
+			return true
+			
+		end
+		
+		if tr.MatType==83 then				//83 is wata
+			local effectdata = EffectData()
+			effectdata:SetOrigin( tr.HitPos )
+			effectdata:SetNormal( tr.HitNormal )		// In case you hit sideways water?
+			effectdata:SetScale( 60 )			// Big splash for big bullets
+			util.Effect( "watersplash", effectdata )
+			self.Entity:Remove()
+			return true
+			
+		end
+		
+		local owner = IsValid( self:GetOwner() ) and self:GetOwner()
+		local inflictor = owner or self.Turret
+		util.BlastDamage(self.Turret, inflictor, tr.HitPos, 400, 100)
+		util.BlastDamage(self.Turret, inflictor, tr.HitPos, 200, 300)
+		
+		local concrete = 67 -- has to be concrete else errors are spammed
+		local effectdata = EffectData()
+		effectdata:SetOrigin(tr.HitPos)				// Position of Impact
+		effectdata:SetNormal(tr.HitNormal)			// Direction of Impact
+		effectdata:SetStart(self.flightvector:GetNormalized())	// Direction of Round
+		effectdata:SetEntity(self.Entity)			// Who done it?
+		effectdata:SetScale(2.2)				// Size of explosion
+		effectdata:SetRadius( concrete )			// Texture of Impact
+		effectdata:SetMagnitude(16)				// Length of explosion trails	
+		util.Effect( "gdca_cinematicboom_t", effectdata )
+		util.ScreenShake(tr.HitPos, 10, 5, 1, 1300 )
+		util.Decal("Scorch", tr.HitPos + tr.HitNormal, tr.HitPos - tr.HitNormal)
+		self.Entity:Remove()	
+		
+	end
+	
+	self.Entity:SetPos(self.Entity:GetPos() + self.flightvector)
+	self.flightvector = self.flightvector + ( Vector(math.Rand(-0.1,0.1), math.Rand(-0.1,0.1),math.Rand(-0.1,0.1)) + Vector(0,0,-0.01) )
+	self.Entity:SetAngles(self.flightvector:Angle())
+	self.Entity:NextThink( CurTime() )
+	return true
+	
+end
+ 
+ 
