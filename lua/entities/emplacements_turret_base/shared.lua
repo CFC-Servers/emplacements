a79b4cf95dd2cc9dab5da6f5923eaeaf8a9ef8a0
StrawWagen <64710817+StrawWagen@users.noreply.github.com>
2021-08-17 21:16:23 -0700

    Change disconnect distance
    Increase the emplacenent disconnect distance from 60 units to 90
    Distance of 60 is too small, and doesn't feel good to play with.
    The proposed new distance, 90, is a bit bigger, without being too out there.

--- lua/entities/emplacements_turret_base/shared.lua
+++ lua/entities/emplacements_turret_base/shared.lua
@@ -58,7 +58,7 @@ function ENT:ShooterStillValid()
         shooter = self:GetDTEntity( 0 )
     end
 
-    return IsValid( shooter ) and shooter:Alive() and ( ( self:GetPos() + self.TurretModelOffset ):Distance( shooter:GetShootPos() ) <= 60 )
+    return IsValid( shooter ) and shooter:Alive() and ( ( self:GetPos() + self.TurretModelOffset ):Distance( shooter:GetShootPos() ) <= 90 )
 end
 
 function ENT:Use( plr )
