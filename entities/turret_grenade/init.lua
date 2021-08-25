079c8871bd059f018e39f35505675d26cdb76f42
StrawWagen <64710817+StrawWagen@users.noreply.github.com>
2021-08-25 13:52:10 -0700

    more changes
    reverted most of the weird dps system
    rewrote the setup check function to ensure compatibility with short setup-time emplacements
    made effects on both bullet turrets make more sense
    made both projectile entities work fine standalone from a turret
    partially reverted last changes to the rail_shell, effect is a bit bigger and increased wide explosion's radius by 50
    many other small tweaks and bugfixes

---
+++ entities/turret_grenade/init.lua
@@ -0,0 +1,9 @@
+AddCSLuaFile( "shared.lua" )
+include( "shared.lua" )
+
+ENT.turretBaseModel = "models/turret/gl_stand.mdl"
+ENT.turretModel = "models/turret/gl_turret.mdl"
+ENT.soundName = "nade_turret"
+ENT.soundPath = "weapons/grenade_launcher1.wav"
+ENT.turretPos = 44.5
+ENT.turretInitialAngle = -90
