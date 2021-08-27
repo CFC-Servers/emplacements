8ece04f26aaf32aa12e403ef5f0d8291ddfde646
StrawWagen <64710817+StrawWagen@users.noreply.github.com>
2021-08-27 11:32:08 -0700

    delete misplaced code

--- entities/turret_bullets/init.lua
+++
@@ -1,9 +0,0 @@
-AddCSLuaFile( "shared.lua" )
-include( "shared.lua" )
-
-ENT.turretBaseModel = "models/reach/weapons/turret/tripod.mdl"
-ENT.turretModel = "models/reach/weapons/turret/hmg_fix.mdl"
-ENT.soundName = "hmg_fire_turret"
-ENT.soundPath = "hturret/discharge.wav"
-ENT.turretPos = 40
-ENT.turretInitialAngle = 90
--- entities/turret_bullets2/init.lua
+++
@@ -1,9 +0,0 @@
-AddCSLuaFile( "shared.lua" )
-include( "shared.lua" )
-
-ENT.turretBaseModel = "models/turret/mg_stand.mdl"
-ENT.turretModel = "models/turret/mg_turret.mdl"
-ENT.soundName = "hmg_heavy_turret"
-ENT.soundPath = "hturret/m249-1.wav"
-ENT.turretPos = 42.2
-ENT.turretInitialAngle = -90
